6eb25e53e42dd78fca2517d35b88a1a28ee57030
kaneeldias <kaneeldias@hotmail.com>
2025-07-26 13:56:37 +0530

    Add page time tracking functionality to enhance analytics

---
+++ src/record-page-time.js
@@ -0,0 +1,17 @@
+const pageLoadTime = Date.now();
+
+window.addEventListener('beforeunload', () => {
+    const pageLeaveTime = Date.now();
+    const timeSpent = (pageLeaveTime - pageLoadTime)
+
+    fetch(`${CONFIG.ANALYTICS_SERVICE_URL}/record-page-time`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            path: window.location.pathname,
+            timeSpent: timeSpent
+        })
+    }).then(r => r.json())
+});
